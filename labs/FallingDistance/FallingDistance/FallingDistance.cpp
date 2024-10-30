ff7381391c3fed821002715818b3da70cf573a4f
August Strain <august.strain@my.tccd.edu>
2024-10-30 00:11:07 -0500

    Updated github

--- labs/FallingDistance/FallingDistance/FallingDistance.cpp
+++ labs/FallingDistance/FallingDistance/FallingDistance.cpp
@@ -5,6 +5,15 @@
 #include <iostream>
 #include <iomanip>
 
+    double fallingDistance(int t)
+    {
+        double d = 0;
+        const double g = 9.8;
+
+        d = (0.5 * g) * (pow(t,2));
+        return d;
+    } 
+
 int main()
 {
     std::cout << "Lab 3\n";
@@ -29,4 +38,13 @@ int main()
         
         std::cout << "ERROR: Falling Time must be between 1 and 60 seconds " << std::endl;
     } while (true);
+
+    int t = 9.8;
+    double d = 0;
+
+    for (int i = 1; i <= fallingTime; i++)
+    {
+        d = fallingDistance(i);
+        std::cout << i << " seconds: " << d << " meters\n ";
+    }
 }
